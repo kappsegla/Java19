26f40b64d8d2ccee03483301fe9bf67141f06f3e
Martin Blomberg <martin.blomberg@outlook.com>
2019-08-22 13:54:46 +0200

    For and while loop example

--- src/LoopExample.java
+++ src/LoopExample.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class LoopExample {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        //for loop that runs 3 times
+        for (int i = 0; i < 3; i++) {
+            String textInput = input.nextLine();
+            System.out.println(i + ": " + textInput);
+        }
+
+        //for loop that runs 3 times
+        for (int i = 0; i < 3; ) {
+            String textInput = input.nextLine();
+            System.out.println(i + ": " + textInput);
+            i++;
+        }
+
+
+        int i = 0;
+        while( i < 3 ){
+            String textInput = input.nextLine();
+            System.out.println(i + ": " + textInput);
+            i++;
+        }
+
+
+    }
 }
