442585151b846e44083a86e74725ae6053b2aff6
Martin Blomberg <martin.blomberg@outlook.com>
2019-08-27 09:23:25 +0200

    Övningsexempel uppg13

--- src/ovn/Uppg13.java
+++ src/ovn/Uppg13.java
@@ -1,11 +1,20 @@
 package ovn;
 
+import java.util.Scanner;
+
 public class Uppg13 {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.print("Input a string: ");
+        String input = scanner.nextLine();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            System.out.print(input.charAt(i));
+        }
+
     }
 
 }
