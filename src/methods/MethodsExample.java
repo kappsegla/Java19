2a890647c52bea4e41ba97e2d0f6b150f663d325
Martin Blomberg <martin.blomberg@outlook.com>
2019-08-27 14:34:20 +0200

    calculateTax

--- src/methods/MethodsExample.java
+++ src/methods/MethodsExample.java
@@ -1,12 +1,33 @@
 package methods;
 
+import java.util.Scanner;
+
 public class MethodsExample {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         //int x = 2;
         //printMultiplicationTable(x);
-        double sum = add(1,2);
-        System.out.println(sum);
+        //double sum = add(1,2);
+
+        double tax = calculateTax(230000);
+        System.out.println(tax);
+
+        String input = scanner.nextLine();
+
+        int income = Integer.parseInt(input);
+        System.out.println(calculateTax(income));
+
     }
+
+    public static double calculateTax(int income) {
+        if (income > 100000) {
+            return income * 0.4;
+        } else {
+            return income * 0.3;
+        }
+    }
+
 
     public static void printMultiplicationTable(int tableNumber) {
         for (int i = 1; i <= 10; i++) {
@@ -14,18 +35,17 @@ public class MethodsExample {
         }
     }
 
-    public static int add(int x, int y){
+    public static int add(int x, int y) {
         return x + y;
     }
 
-    public static int add(int x, int y, int z){
+    public static int add(int x, int y, int z) {
         return x + y + z;
     }
 
-    public static double add(double x, double y){
+    public static double add(double x, double y) {
         return x + y;
     }
-
 
 
 }
