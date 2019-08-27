29b8b242bd2377e00ba1439d8b9e743ecf099704
Martin Blomberg <martin.blomberg@outlook.com>
2019-08-27 10:40:14 +0200

    Övningsexempel uppg20

--- src/ovn/Uppg20.java
+++ src/ovn/Uppg20.java
@@ -8,20 +8,32 @@ public class Uppg20 {
 
         String input = scanner.nextLine();
         double tal1 = Float.parseFloat(input);
-
+        //tal1 = tal1 - (int)tal1;
         input = scanner.nextLine();
         double tal2 = Float.parseFloat(input);
+        //tal2 = tal2 - (int)tal2;
 
         double diff = Math.abs(tal1 - tal2);
-        System.out.println(diff);
+       // System.out.println(diff);
 
-        if (diff < 0.001) {
-            System.out.println("Talen är lika till 3 decimaler");
-        } else {
-            System.out.println("Talen är olika till 3 decimaler");
+//        if (diff < 0.001) {
+//            System.out.println("Talen är lika till 3 decimaler");
+//        } else {
+//            System.out.println("Talen är olika till 3 decimaler");
+//        }
+
+        int heltal1 = (int)(tal1 * 1000);
+
+        int heltal2 = (int) (tal2 * 1000);
+
+        System.out.println(heltal2);
+        if( heltal1 == heltal2)
+        {
+            System.out.println("lika");
         }
-
-
-
+        else
+        {
+            System.out.println("olika");
+        }
     }
 }
