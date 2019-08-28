667c90a21f6607a27f8368344bc4ce348b010d92
Martin Blomberg <martin.blomberg@outlook.com>
2019-08-28 11:43:48 +0200

    OOP Rectangle

--- src/OOPExample.java
+++ src/OOPExample.java
@@ -1,12 +1,13 @@
 public class OOPExample {
 
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle();
+        Rectangle rectangle = new Rectangle(10,10,"Name");
 
         Rectangle rectangle1 = new Rectangle();
 
-        rectangle.setHeight(-10);
         rectangle1.setHeight(20);
+
+
 
         System.out.println(rectangle.getHeight());
         System.out.println(rectangle1.getHeight());
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -4,14 +4,39 @@ public class Rectangle {
     private int height;
     private String name;
 
-    public void setHeight(int h) {
-        if (h > 0)
-            height = h;
+    public Rectangle() {
+        name = "";
+    }
+
+    public Rectangle(int width, int height, String name) {
+        this.width = width;
+        this.height = height;
+        this.name = name;
+    }
+
+    public void setHeight(int height) {
+        if (height > 0)
+            this.height = height;
     }
 
     public int getHeight() {
         return height;
     }
 
+    public int getWidth() {
+        return width;
+    }
 
+    public void setWidth(int width) {
+        if (width > 0)
+            this.width = width;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
