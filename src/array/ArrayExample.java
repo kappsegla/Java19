31131cd1329f9080b7116f5734e8bb5d868259d5
Martin Blomberg <martin.blomberg@outlook.com>
2019-08-29 12:27:28 +0200

    StringList

--- src/array/ArrayExample.java
+++ src/array/ArrayExample.java
@@ -1,41 +1,52 @@
 package array;
 
-import java.awt.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayExample {
 
     public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Press enter to start allocation");
+//        scanner.nextLine();
+//
+//        //To be able to create this max array size use VM flag -Xmx10g when starting application
+//        int[] big = new int[Integer.MAX_VALUE-8];
+//        System.out.println("Big array allocated");
+//        System.out.println("Press enter to release array");
+//        scanner.nextLine();
+//        //big = null;
+//        //System.gc();
+//        scanner.nextLine();
+
 //        int[] array = new int[4];
-//
 //        array[0] = 10;
-//
 //        System.out.println(array[0]);
 //        System.out.println(array.length);
 //
-//        String s = "Hej";
-//        s.length();
         //For each loop
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 
-        Scanner scanner = new Scanner(System.in);
-        String searchString = scanner.nextLine();
+        for (String car : cars) {
+            System.out.print(car);
+            if (car.length() < 4) {
+                System.out.print("\t");
+            }
+            System.out.print("\t");
+        }
+    }
 
-        int index = findIndexOfCar(searchString, cars);
-        System.out.println(index);
+    public static void print2DimArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.println(array[i][j]);
+            }
+        }
 
-//        for (String car : cars) {
-//            System.out.print(car);
-//            if( car.length() < 4) {
-//                System.out.print("\t");
-//            }
-//            System.out.print("\t");
     }
 
     private static int findIndexOfCar(String brand, String[] array) {
         for (int i = 0; i < array.length; i++) {
-            if( brand.equals( array[i] ) )
+            if (brand.equals(array[i]))
                 return i;
         }
         return -1;
---
+++ src/array/StringList.java
@@ -0,0 +1,37 @@
+package array;
+
+public class StringList {
+
+    private String[] array;
+
+    public StringList() {
+
+    }
+
+    /**
+     * Adds string s to end of list
+      * @param s String to add
+     */
+    public void add(String s){
+
+    }
+
+    /**
+     * Returns the string stored at index
+     * @param index Used for indexing into the list, >= 0
+     * @return String reference stored at index
+     */
+    public String get(int index){
+
+    }
+
+    /**
+     * @return Number of String references stored in list
+     */
+    public int size(){
+
+    }
+
+
+
+}
