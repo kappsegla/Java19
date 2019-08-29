3895c60b440b521e935fa933293a1c1ce91860e4
Martin Blomberg <martin.blomberg@outlook.com>
2019-08-29 13:10:01 +0200

    StringListTest

--- src/array/StringList.java
+++ src/array/StringList.java
@@ -1,37 +1,57 @@
 package array;
 
+import java.util.Arrays;
+
 public class StringList {
 
     private String[] array;
+    private int count;
 
     public StringList() {
+        //Create an array
+        array = new String[10];
+    }
 
+    public StringList(int startSize) {
+        //Create an array
+        array = new String[startSize];
     }
 
     /**
      * Adds string s to end of list
-      * @param s String to add
+     *
+     * @param s String to add
      */
-    public void add(String s){
+    public void add(String s) {
+        if (count > array.length - 1) {
+            //Create new array with more space
+            String[] newArray = new String[array.length + (int)(array.length * 0.5)];
+            //Copy old array to new array
+            
 
+            //Remove old array
+
+        }
+        array[count] = s;
+        count++;
     }
 
     /**
-     * Returns the string stored at index
-     * @param index Used for indexing into the list, >= 0
-     * @return String reference stored at index
+     * Returns the element at the specified position
+     *
+     * @param index Position of element, >= 0
+     * @return Reference to element
      */
-    public String get(int index){
-
+    public String get(int index) {
+        return array[index];
     }
 
     /**
-     * @return Number of String references stored in list
+     * Returns the number of elements stored in this list.
+     *
+     * @return Number of elements
      */
-    public int size(){
-
+    public int size() {
+        return count;
     }
-
-
-
 }
---
+++ src/array/StringListTest.java
@@ -0,0 +1,24 @@
+package array;
+
+public class StringListTest {
+
+    public static void main(String[] args) {
+
+        StringList list1 = new StringList();
+        StringList list2 = new StringList(2);
+
+        list1.add("First String");
+        String s = list1.get(0);
+        int size = list1.size();
+
+        System.out.println(s);  //First String
+        System.out.println(size); //1
+
+        list2.add("First");
+        list2.add("Second");
+        list2.add("Third");
+
+
+
+    }
+}
