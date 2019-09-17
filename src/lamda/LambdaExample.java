cbe6f7f152cd15b7e27ec75d4240a1eb0637d024
Martin Blomberg <martin.blomberg@outlook.com>
2019-09-17 13:40:04 +0200

    Comparator as Lambda

--- src/lamda/LambdaExample.java
+++ src/lamda/LambdaExample.java
@@ -1,6 +1,8 @@
 package lamda;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class LambdaExample {
@@ -15,6 +17,23 @@ public class LambdaExample {
         persons.add(new Person("Stina", 27));
         persons.add(new Person("Maria", 15));
 
+        //Implementation of Comparator as own class
         persons.sort(new SortPersonsByName());
+
+        //Anonymous class implementation
+        persons.sort(new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        });
+
+        //Lambda implementation of functional interface Comparator
+        persons.sort((o1, o2) -> o2.getAge() - o1.getAge());
+
+
+
+        System.out.println(persons);
+
     }
 }
