24b7a5c714f76f10117d0e9587b8db9a2889888a
Martin Blomberg <martin.blomberg@outlook.com>
2019-09-19 13:18:18 +0200

    Either.lift

--- src/exceptions/StreamsAndExceptions.java
+++ src/exceptions/StreamsAndExceptions.java
@@ -1,18 +1,33 @@
 package exceptions;
 
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class StreamsAndExceptions {
 
 
     public static void main(String[] args) {
-
         var stream = Stream.of("1", "2", "3", "4", "5", "6", "a", "b", "c");
 
         long i = 0;
-        i = stream.mapToInt(s -> wrapper(s)).sum();
+      //  i = stream.mapToInt( s->wrapper(s) ).sum();
+      //  i = stream.map( wrap(  s -> parser(s)) ).mapToInt(Integer::intValue).sum();
+   //     i = stream.mapToInt(s -> wrapper(s)).sum();
+   //     i = stream.map(wrap( s -> parser(s))).mapToInt(Integer::intValue).sum();
+
+        //Uses a Either monad
+        //https://en.wikipedia.org/wiki/Monad_(functional_programming)
+        //stream.map(  Either.lift(s -> parser(s))  ).forEach(System.out::println);
+        i =  stream.map(  Either.lift(s -> parser(s))  )
+                .filter(either -> either.isRight())
+                .map(s-> s.getRight())
+                .mapToInt(s -> (Integer) s.orElse(0))
+                .sum();
 
         System.out.println(i);
+    }
+    public static Integer parser(String s) throws Exception {
+        return Integer.parseInt(s);
     }
 
     public static int wrapper(String s) {
@@ -24,7 +39,15 @@ public class StreamsAndExceptions {
         }
     }
 
-    public static int parser(String s) throws Exception {
-        return Integer.parseInt(s);
+    public static <T,R> Function<T,R> wrap(CheckedFunction<T,R> checkedFunction) {
+        return t -> {
+            try {
+                return checkedFunction.apply(t);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
+
+
 }
