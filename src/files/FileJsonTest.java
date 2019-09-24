be9b917d0dab287100eed87cf5003bc4d1ae158c
Martin Blomberg <martin.blomberg@outlook.com>
2019-09-24 15:37:11 +0200

    JSON load

--- src/files/FileJsonTest.java
+++ src/files/FileJsonTest.java
@@ -1,50 +1,60 @@
 package files;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileJsonTest {
 
-        public static void main(String[] args) {
-            String path = System.getProperty("user.home") +
-                    File.separator + "Documents" +
-                    File.separator + "CustomFolder";
+    public static void main(String[] args) {
+        String path = System.getProperty("user.home") +
+                File.separator + "Documents" +
+                File.separator + "CustomFolder";
 
-            File dir = new File(path);
+        File dir = new File(path);
 
-            if (dir.exists())
-                System.out.println("Folder exists");
-            else if (dir.mkdir())
-                System.out.println("Folder created");
-            else
-                System.out.println("Folder not created");
+        if (dir.exists())
+            System.out.println("Folder exists");
+        else if (dir.mkdir())
+            System.out.println("Folder created");
+        else
+            System.out.println("Folder not created");
 
-            File filePath = new File(path + File.separator + "savefile.json");
+        File filePath = new File(path + File.separator + "savefile.json");
 
-            List<Person> persons = new ArrayList<>();
+        List<Person> persons = new ArrayList<>();
+        Gson gson = new Gson();
 
-            if (filePath.exists()) {
-                //TODO: Load from Json file
+        if (filePath.exists()) {
 
-            }
-            System.out.println(persons);
-
-            persons.add(new Person("Martin", 402343533));
-
-            //TODO: Save as Json to file
-            Gson gson = new Gson();
-            String json = gson.toJson(persons);
+            Type fooType = new TypeToken<ArrayList<Person>>() {
+            }.getType();
 
             try {
-                FileWriter out = new FileWriter(filePath);
-                out.write(json);
-                out.close();
-            } catch (IOException e) {
+                FileReader fileReader = new FileReader(filePath);
+                persons = gson.fromJson(fileReader, fooType);
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
+        System.out.println(persons);
+
+        persons.add(new Person("Martin", 402343533));
+
+        String json = gson.toJson(persons);
+
+        try {
+            FileWriter out = new FileWriter(filePath);
+            out.write(json);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+}
 
