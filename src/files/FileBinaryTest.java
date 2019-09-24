221baef94c1465a11343553ade3445b37df526f5
Martin Blomberg <martin.blomberg@outlook.com>
2019-09-24 13:53:43 +0200

    binary save and load of List

--- src/files/FileBinaryTest.java
+++ src/files/FileBinaryTest.java
@@ -7,43 +7,56 @@ import java.util.Scanner;
 
 public class FileBinaryTest {
 
-        public static void main(String[] args) {
-            String path = System.getProperty("user.home") +
-                    File.separator + "Documents" +
-                    File.separator + "CustomFolder";
+    public static void main(String[] args) {
+        String path = System.getProperty("user.home") +
+                File.separator + "Documents" +
+                File.separator + "CustomFolder";
 
-            File dir = new File(path);
+        File dir = new File(path);
 
-            if (dir.exists())
-                System.out.println("Folder exists");
-            else if (dir.mkdir())
-                System.out.println("Folder created");
-            else
-                System.out.println("Folder not created");
+        if (dir.exists())
+            System.out.println("Folder exists");
+        else if (dir.mkdir())
+            System.out.println("Folder created");
+        else
+            System.out.println("Folder not created");
 
-            File filePath = new File(path + File.separator + "savefile.bin");
+        File filePath = new File(path + File.separator + "savefile.bin");
 
-            List<Person> persons = new ArrayList<>();
+        List<Person> persons = new ArrayList<>();
 
-            if (filePath.exists()) {
-
-                //Read file here
-            }
-            System.out.println(persons);
-
-            persons.add(new Person("Martin", 402343533));
-
+        if (filePath.exists()) {
             try {
-                FileOutputStream outputStream = new FileOutputStream(filePath);
-                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+                FileInputStream inputStream = new FileInputStream(filePath);
+                DataInputStream dataInputStream = new DataInputStream(inputStream);
 
-                for (Person p : persons) {
-                    dataOutputStream.writeUTF(p.getName());
-                    dataOutputStream.writeInt(p.getAge());
+                while( dataInputStream.available() > 0 ) {
+                    String name = dataInputStream.readUTF();
+                    int age = dataInputStream.readInt();
+                    Person person = new Person(name,age);
+                    persons.add(person);
                 }
-                dataOutputStream.close();
+                dataInputStream.readInt();
+                dataInputStream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        System.out.println(persons);
+
+        persons.add(new Person("Martin", 402343533));
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(filePath);
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+            for (Person p : persons) {
+                dataOutputStream.writeUTF(p.getName());
+                dataOutputStream.writeInt(p.getAge());
+            }
+            dataOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+}
