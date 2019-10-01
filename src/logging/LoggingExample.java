d857523eae3d14f8ad9cd4bafc14482cfb4ed7b5
Martin Blomberg <martin.blomberg@outlook.com>
2019-10-01 10:46:16 +0200

    log4j xml config file

--- src/logging/LoggingExample.java
+++ src/logging/LoggingExample.java
@@ -1,5 +1,6 @@
 package logging;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,20 +15,28 @@ public class LoggingExample {
     //Include log4j library in project.http://logging.apache.org/log4j/2.x/download.html
     //log4j-api-2.12.1.jar
     //log4j-core-2.12.1.jar
-    private static final Logger logger = LogManager.getLogger(LoggingExample.class.getName());
+    private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
 
-        try (InputStream inputStream = LoggingExample.class.getResourceAsStream("/file2.txt")) {
+        try (InputStream inputStream = LoggingExample.class.getResourceAsStream("/file.txt")) {
             Scanner sc = new Scanner(inputStream);
-            while (sc.hasNext())
+            while (sc.hasNext()) {
                 System.out.println(sc.nextLine());
+                logger.debug("Found a line in file");
+            }
         } catch (IOException e) {
             //TODO: log error
             logger.error(e.getMessage());
         }
         catch (NullPointerException e) {
+            logger.fatal("Death to the program");
             logger.error("Null Pointer Exception in main method");
+            logger.debug("This is a debug message");
+            logger.warn("Warning!");
+            logger.info("Info message");
+
+            logger.log(Level.ERROR,"Logging error level with log method");
         }
     }
 }
