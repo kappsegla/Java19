29b1b322636471e9aa06b2bc84d6159f585f03a3
Martin Blomberg <martin.blomberg@outlook.com>
2019-10-01 13:50:27 +0200

    Parallell Stream

--- src/threads/ParallellStream.java
+++ src/threads/ParallellStream.java
@@ -1,11 +1,25 @@
 package threads;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class ParallellStream {
 
     public static void main(String[] args) {
 
-        
+        int ints = IntStream.range(0,10000)
+                .parallel()
+                .peek(ParallellStream::printInfo)
+                .filter( x -> x < 5)
+                .sum();
 
+        System.out.println(ints);
+    }
 
+    private static void printInfo(Integer x) {
+        System.out.println(x);
+        System.out.println(Thread.currentThread().getName());
     }
 }
