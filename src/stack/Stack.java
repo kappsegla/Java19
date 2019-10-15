5210f5bc2a400296e89b5c894693314c7622b2e5
Martin Blomberg <martin.blomberg@outlook.com>
2019-10-15 12:01:00 +0200

    Stack with Tests

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,4 +1,30 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
+
 public class Stack {
+    List<String> list = new ArrayList<>();
+
+    public void push(String text) {
+        list.add(text);
+    }
+
+    public int count() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public String pop() {
+        int lastIndex = list.size() -1;
+        if( lastIndex < 0)
+            throw new EmptyStackException();
+        String s = list.get(lastIndex);
+        list.remove(lastIndex);
+        return s;
+    }
 }
--- test/stack/StackTest.java
+++ test/stack/StackTest.java
@@ -1,14 +1,43 @@
 package stack;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackTest {
 
     @Test
-    void testTest()
-    {
-        assertTrue(true);
+    void pushToStackSizeIncreasesByOne() {
+        Stack stack = new Stack();
+        int startSize = stack.count();
+
+        stack.push("Text");
+
+        assertEquals(startSize + 1, stack.count());
+    }
+
+    @Test
+    void newListIsEmpty() {
+        Stack stack = new Stack();
+
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Pop from Stack returns last inserted object")
+    void popFromStackReturnsLastAddedObject() {
+        Stack stack = new Stack();
+        stack.push("First");
+        stack.push("LIFO");
+        assertEquals("LIFO", stack.pop());
+    }
+
+    @Test
+    void popFromEmptyStackThrowsEmptyStackException() {
+        Stack stack = new Stack();
+        assertThrows(EmptyStackException.class, () -> stack.pop());
     }
 }
