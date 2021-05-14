4de61cf3b8ceeb887cf98dfd6a5aa7cd5533d301
Martin Blomberg <martin.blomberg@outlook.com>
2021-05-14 19:08:57 +0200

    More fizzbuzz

---
+++ src/main/java/functional/Functional.java
@@ -0,0 +1,28 @@
+package functional;
+
+public class Functional {
+
+
+    public static void main(String[] args) {
+        new Functional().run();
+    }
+
+    public void run(){
+
+
+
+
+    }
+
+    void lambdaTest() {
+        testNPE(null);
+    }
+
+    private static void testNPE(String s) {
+//invocation target of a method reference is evaluated when its declaration is first encountered
+//        Thread t = new Thread(() -> s.toLowerCase());
+        Thread t = new Thread(s::toLowerCase);
+        t.setUncaughtExceptionHandler((t1, e) -> System.out.println("Exception!"));
+        t.start();
+    }
+}
